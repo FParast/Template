9670e25318fcf761be84f7130b08e2c34b3765ff
FKParast <khodapaf@uoguelph.ca>
2025-05-05 18:51:46 -0300

    try(1)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 
 def add(a, b):
-    # TODO: Complete this function
-    return a + b  # Example correct implementation
+    c = a + b
+    return c  # Example correct implementation
 
 # Optional: Add more functions if needed
